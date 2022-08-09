b4685a5f451fc09acf3d9851fb888b93abaf7412
kkm213 <kkm213@korea.ac.kr>
2022-08-09 16:35:54 +0900

    upload

--- execute.sh
+++ execute.sh
@@ -3,5 +3,5 @@ ghdl -a fa.vhd
 ghdl -a adder.vhd
 ghdl -a adder_tb.vhd
 ghdl -e adder_tb
-ghdl -r adder_tb --stop-time=200ns --wave=adder_tb.ghw
+ghdl -r adder_tb --stop-time=120ns --wave=adder_tb.ghw
 gtkwave adder_tb.ghw
