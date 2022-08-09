0923ca56352aa15f9ab027a5c039c9aca82fd84f
kkm213 <kkm213@korea.ac.kr>
2022-08-09 16:32:35 +0900

    upload

--- adder_tb.vhd
+++ adder_tb.vhd
@@ -22,17 +22,11 @@ begin
         a_s <= "1011001101001010";
         b_s <= "1010100101110111";
         wait for 40 ns;
-    end process;
-    
-    calc2 : process
-    begin
+
         a_s <= "0001111011001101";
         b_s <= "1110101100011110";
-        wait for 40 ns;
-    end process;
+        wait for 40 ns
 
-    calc3 : process
-    begin
         a_s <= "0010110011111000";
         b_s <= "0110101011101011";
         wait for 40 ns;
